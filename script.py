f632990f2b5ae3716beb8e9bf148141b96c3bc52
Semjon Kravtšenko <semjon.00@gmail.com>
2020-12-14 02:01:56 +0200

    1

--- script.py
+++ script.py
@@ -1,13 +1,7 @@
 import pandas as pd
-import numpy as np
-import sklearn
-from matplotlib import pyplot as plt
-import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import MinMaxScaler
 import pickle
 
 
